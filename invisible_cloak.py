6f4dc0c66e58fa59eef98ba216989573406382f9
Poorva870 <80689366+Poorva870@users.noreply.github.com>
2022-10-04 18:59:04 +0530

    Update invisible_cloak.py

--- invisible_cloak.py
+++ invisible_cloak.py
@@ -1,64 +1,31 @@
-import cv2
-import time
-import numpy as np
+import cv2 
+import numpy as np 
 
-#To save the output in a file output.avi
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-output_file = cv2.VideoWriter('output.avi', fourcc, 20.0, (640, 480))
+video = cv2.VideoCapture(0) 
+image = cv2.imread("me.jpeg") 
 
-#Starting the webcam
-cap = cv2.VideoCapture(0)
+while True:
+    ret, frame = video.read() 
+    print(frame) 
 
-#Allowing the webcam to start by making the code sleep for 2 seconds
-time.sleep(2)
-bg = 0
+    frame = cv2.resize(frame, (640, 480)) 
+    image = cv2.resize(image, (640, 480)) 
 
-#Capturing background for 60 frames
-for i in range(60):
-    ret, bg = cap.read()
-#Flipping the background
-bg = np.flip(bg, axis=1)
+    u_black = np.array([104, 153, 70]) 
+    l_black = np.array([30, 30, 0]) 
 
-#Reading the captured frame until the camera is open
-while (cap.isOpened()):
-    ret, image, frame = cap.read()
-    if not ret:
-        break
+    mask = cv2.inRange(frame, l_black, u_black) 
+    
+    res = cv2.bitwise_and(frame, frame, mask = mask) 
 
-     #Flipping the image for consistency
-    image = np.flip(image, axis=1)
-    frame = np.flip(frame, axis=1)
+    f = frame - res 
+    f = np.where(f == 0, image, f) 
 
-    #Resizing the frame for consistency
-    frame = cv2.resize(frame, (640, 480))
-    image = cv2.resize(image, (640, 480))
+    cv2.imshow("video", frame) 
+    cv2.imshow("mask", f) 
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'): 
+        break 
+    video.release() 
 
-    #Generating mask to detect red colour
-    #These values can also be changed as per the color
-    l_black = np.array([104, 153, 70])
-    u_black = np.array([30, 30,0])
-
-    mask = cv2.inRange(frame, l_black, u_black)
-
-    #Open and expand the image where there is mask 1 (color)
-    mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, np.ones((3, 3), np.uint8))
-
-    #Keeping only the part of the images without the red color 
-    #(or any other color you may choose)
-    res = cv2.bitwise_and(frame, frame, mask=mask)
-
-    f = frame - res
-    f= np.where(f == 0, image, f)
-
-    #Generating the final output
-    final_output = cv2.addWeighted(res, 1)
-    output_file.write(final_output)
-
-    #Displaying the output to the user
-    cv2.imshow("magic", final_output)
-    cv2.waitKey(1)
-
-
-cap.release()
-#out.release()
 cv2.destroyAllWindows()
